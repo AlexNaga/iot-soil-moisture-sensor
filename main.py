16d5ba5f792603fc60fdd0e9e3e139c76c54fc31
Alex <alex.naga@hotmail.com>
2021-07-31 11:37:09 +0200

    feat: send data to Pybytes

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ BLUE = 0x0000FF
 BLACK = 0x000000
 
 
-def check_moist():
-    time.sleep(3)
-    moist_level = (moist_sensor.value_in_millivolt(AO_PIN, VCC_PIN) / 4.096)
-    print('Moisture level {} mV'.format(moist_level))
-
+def get_moist_level():
     # The soil sensor values are between ~250-999 mV, where the lowest value means the plant is moist and 999 is dry
-    if moist_level > 400 and moist_level < 600:
+    moist_level = (moist_sensor.value_in_millivolt(AO_PIN, VCC_PIN) / 4.096)
+    return moist_level
+
+
+def check_moist(moist_level):
+    if moist_level > 400 and moist_level < 800:
         print("The moisture level is OK.")
         pycom.rgbled(GREEN)
     elif moist_level < 400:
@@ -33,9 +34,26 @@ def check_moist():
 
     time.sleep(3)
     pycom.rgbled(BLACK)
+    return moist_level
+
+
+def send_data(moist_level):
+    if moist_level is None:
+        print("Error: No moist level when sending data.")
+        return
+
+    # send data to Pybytes in channel 2
+    pybytes_channel = 2
+    pybytes.send_signal(pybytes_channel, moist_level)
+    print("Sending: {}".format(moist_level))
 
 
 while True:
-    check_moist()
-    time.sleep(2)
-    #time.sleep(60 * 5)
+    moist_level = get_moist_level()
+    print('Moisture level {} mV'.format(moist_level))
+
+    check_moist(moist_level)
+    send_data(moist_level)
+
+    # time.sleep(5)
+    time.sleep(60 * 5)
